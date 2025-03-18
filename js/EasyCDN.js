6ed7219d6ec3e0b8a76b20ba9087e563966cf33b
rakeshkanna-rk <rakeshkanna0108@gmail.com>
2025-03-18 16:28:09 +0530

    debug

--- js/EasyCDN.js
+++ js/EasyCDN.js
@@ -1,12 +1,24 @@
-function EasyCDN(jsonPath, attachToHead = false, jsDefer = false) {
+function EasyCDN(jsonPath, attachToHead = false, callback = null) {
   fetch(jsonPath)
     .then((res) => res.json())
     .then((data) => {
+      let totalFiles = (data.css?.length || 0) + (data.js?.length || 0);
+      let loadedFiles = 0;
+
+      function checkAllLoaded(fileUrl) {
+        console.log("Loaded: " + fileUrl);
+        loadedFiles++;
+        if (loadedFiles === totalFiles && typeof callback === "function") {
+          callback();
+        }
+      }
+
       if (data.css) {
         data.css.forEach((href) => {
           let link = document.createElement("link");
           link.rel = "stylesheet";
           link.href = href;
+          link.onload = () => checkAllLoaded(href);
           document[attachToHead ? "head" : "body"].appendChild(link);
         });
       }
@@ -15,7 +27,7 @@ function EasyCDN(jsonPath, attachToHead = false, jsDefer = false) {
         data.js.forEach((src) => {
           let script = document.createElement("script");
           script.src = src;
-          script.defer = jsDefer; // Ensures non-blocking execution
+          script.onload = () => checkAllLoaded(src);
           document[attachToHead ? "head" : "body"].appendChild(script);
         });
       }
